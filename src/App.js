5838f4ec4ec77af11c60f9e483d74b5a4ee85054
jbielt <jbielturon@gmail.com>
2022-04-03 08:17:18 +0200

    split ExpensesItems to another component called Expenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-function App() {
+function App(props) {
   const expenses = [
     { title: "Toilet Paper", amount: 238.2, date: new Date(2022, 3, 3) },
     { title: "Car travels", amount: 40.8, date: new Date(2022, 3, 24) },
@@ -9,27 +9,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
