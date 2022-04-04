fc18ae0ce616ee1fb8eefee8eeac987f28460d73
jbielt <jbielturon@gmail.com>
2022-04-04 18:30:07 +0200

    updating state that depends on the previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -13,9 +13,12 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+    //setUserInput({
+    //  ...userInput,
+    //  enteredTitle: event.target.value,
+    //});
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
     });
   };
   const amountChangeHandler = (event) => {
