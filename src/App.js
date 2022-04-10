d7b4500cfd8aaaae18eefafcd6b2a5ea51ce17fd
jbielt <jbielturon@gmail.com>
2022-04-10 20:33:14 +0200

    using stateful lists, adding expense dynamic in a array

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 238.2,
+    date: new Date(2022, 3, 3),
+  },
+  {
+    id: "e2",
+    title: "Car travels",
+    amount: 40.8,
+    date: new Date(2022, 3, 24),
+  },
+  { id: "e3", title: "Food", amount: 300, date: new Date(2022, 3, 11) },
+  { id: "e4", title: "Others", amount: 132.5, date: new Date(2022, 3, 7) },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 238.2,
-      date: new Date(2022, 3, 3),
-    },
-    {
-      id: "e2",
-      title: "Car travels",
-      amount: 40.8,
-      date: new Date(2022, 3, 24),
-    },
-    { id: "e3", title: "Food", amount: 300, date: new Date(2022, 3, 11) },
-    { id: "e4", title: "Others", amount: 132.5, date: new Date(2022, 3, 7) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
